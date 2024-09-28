3b333d9a6e8ce974567483d0b48b02910dd4be62
Dagson Gabriel <dagson.g.01@gmail.com>
2024-09-27 23:06:43 -0300

    feat: implementa interação de tabs

---
+++ script.js
@@ -0,0 +1,16 @@
+function openTab(tabName, element) {
+  var i, tabcontent, tablinks;
+  
+  tabcontent = document.getElementsByClassName("tab-content");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].classList.remove("active");
+  }
+
+  tablinks = document.getElementsByClassName("tab-link");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].classList.remove("active");
+  }
+
+  document.getElementById(tabName).classList.add("active");
+  element.classList.add("active");
+}
